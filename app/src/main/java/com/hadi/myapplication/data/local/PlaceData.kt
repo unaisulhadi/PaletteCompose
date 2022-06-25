5935a42510d46b63db48477b619abece486ec55c
Unaisul Hadi <unaisulhadi@gmail.com>
2022-06-25 21:56:30 +0530

    Updated local data

--- app/src/main/java/com/hadi/myapplication/data/local/PlaceData.kt
+++ app/src/main/java/com/hadi/myapplication/data/local/PlaceData.kt
@@ -20,7 +20,7 @@ val placeData = listOf(
         placeCategory = "Backwater",
         description = "Referred to as the Venice of the East, Alappuzha has always enjoyed an important place in the maritime history of Kerala. Today, it is famous for its boat races, backwater holidays, beaches, marine products and coir industry. Alappuzha Beach is a popular picnic spot. The pier, which extends out to the sea here, is over 137 years old. Entertainment facilities at the Vijaya Beach Park add to the attraction of the beach. There is also an old lighthouse nearby which greatly intrigues all visitors.",
         imageUrl = "https://cdn.pixabay.com/photo/2017/04/19/16/01/kerala-houseboat-2242698_960_720.jpg",
-        climate = "20°c Light Fog",
+        climate = "32°c Warm",
         rating = 4.8
     ),
 
@@ -30,7 +30,7 @@ val placeData = listOf(
         placeCategory = "Hill Station",
         description = "Munnar, the famed hill station of south India, Munnar is situated at the confluence of three mountain streams – Muthirapuzha, Nallathanni and Kundala – and the word ‘Munnar’ means three rivers in Malayalam.  Situated about 1600m above sea level, the hill station had been the summer resort of the British Government during the colonial era. Imprints of the colonial past stand tall in the Munnar town in the form of English country cottages. Virgin forests, savannah, rolling hills, scenic valleys, numerous streams, huge splashy waterfalls, sprawling tea plantations and winding walkways are all part of the great holiday experience on offer for a traveler to Munnar.",
         imageUrl = "https://cdn.pixabay.com/photo/2020/01/16/04/52/munnar-4769654_960_720.jpg",
-        climate = "16°c Cloudy",
+        climate = "12°c Cloudy",
         rating = 4.9
     ),
 
@@ -40,7 +40,7 @@ val placeData = listOf(
         placeCategory = "Beach",
         description = "Golden, Groovy, Gumptious – Goa.  A cocktail of tropical influences. An oasis of white-gold sands, azure waters, and kaleidoscopic skies. A playground for historians, hippies, and spiritualists alike. A backpacker’s haven, and a wealthy man’s fancy. An assemblage of little Portuguese bungalows, and a string of luxury hotels - Goa is a heady blend of the old and the new. If not for every reason mentioned above, make a trip solely for its succulent cuisine.",
         imageUrl = "https://cdn.pixabay.com/photo/2020/02/10/08/19/zzz-4835696_960_720.jpg",
-        climate = "28°c Clear",
+        climate = "32°c Clear",
         rating = 4.7
     )
 
